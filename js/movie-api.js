5563ee3637d0e7b234896a0a401cbecef8c8c383
JMComstock <jasoncomstock11@gmail.com>
2022-02-09 12:18:21 -0600

    called api, started basic format for html

--- js/movie-api.js
+++ js/movie-api.js
@@ -1,2 +1,28 @@
 "use strict";
 
+// fetch("https://ajar-energetic-louse.glitch.me/movies")
+// .then(res => res.json())
+// .then(resObj => console.log(resObj))
+
+// alert("Loading....");
+let body = $("body");
+fetch("https://ajar-energetic-louse.glitch.me/movies")
+    .then(res => res.json())
+    .then(resObj => {
+        console.log(resObj)
+        resObj.forEach(element =>  {
+            body.append(`
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h2 class="card-title">${element.title} - (${element.year})</h2>
+                        <h4>Director: ${element.director}</h4>
+                        <p class="card-text">${element.plot}</p>
+                    </div>
+                </div>               
+`)
+
+
+        })
+        }
+    )
+
