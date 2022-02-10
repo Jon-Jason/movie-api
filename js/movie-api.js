7c04b8a74f87724cd6437f70563bff41f8dafa97
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-02-10 10:05:05 -0600

    movie api progress

--- js/movie-api.js
+++ js/movie-api.js
@@ -5,24 +5,24 @@
 // .then(resObj => console.log(resObj))
 
 // alert("Loading....");
-let body = $("body");
+let movies = $("#movies");
 fetch("https://ajar-energetic-louse.glitch.me/movies")
     .then(res => res.json())
     .then(resObj => {
         console.log(resObj)
         resObj.forEach(element =>  {
-            body.append(`
+            movies.append(`
+                <div class="">
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
                         <h2 class="card-title">${element.title} - (${element.year})</h2>
-                        <h4>Director: ${element.director}</h4>
+                        <h4 class="card-text">Rating: ${element.rating} Stars</h4>
+                        <h5>Director: ${element.director}</h5>
                         <p class="card-text">${element.plot}</p>
                     </div>
-                </div>               
-`)
-
-
-        })
-        }
-    )
+                </div>       
+                </div>        
+            `)
+        })}
+    );
 
